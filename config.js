d5986aca8e24ea79354b555d5cc31b716bf04d13
theblackstaginn <thefarm1965@gmail.com>
2026-09-22 00:53:20 -0400

    Update config.js

--- config.js
+++ config.js
@@ -31,7 +31,7 @@ window.BLACK_STAG_CONFIG = {
       Example format:
       https://xxxxxxxxxxxxxxxxxxxx.supabase.co
     */
-    url: "https://lpjpuedylongafgqbwjh.supabase.co/rest/v1/",
+    url: "https://lpiquedvloneafegbwih.supabase.co",
 
     /*
       Paste ONLY the Publishable key from:
